03cc09120ddca6fffbdd275b43905c5a65a50701
rootazero <zouguojunx@gmail.com>
2026-01-28 21:58:58 +0800

    routing: add hierarchical route resolver with binding priority

--- core/src/routing/mod.rs
+++ core/src/routing/mod.rs
@@ -4,7 +4,9 @@
 
 pub mod config;
 pub mod identity_links;
+pub mod resolve;
 pub mod session_key;
 
 pub use config::{MatchRule, PeerMatchConfig, RouteBinding, SessionConfig};
+pub use resolve::{resolve_route, MatchedBy, ResolvedRoute, RouteInput, RoutePeer, RoutePeerKind};
 pub use session_key::{normalize_agent_id, DmScope, PeerKind, SessionKey, DEFAULT_AGENT_ID, DEFAULT_MAIN_KEY};
