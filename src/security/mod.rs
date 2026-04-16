e5d9ea5ff43f84f007e06dfeb3deb9622c79380e
rootazero <zouguojunx@gmail.com>
2026-04-16 19:58:40 +0800

    security: export RuntimeSecurityGuard and related types

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -11,3 +11,16 @@ pub mod content_sanitizer;
 pub mod headers;
 pub mod runtime_guard;
 pub mod ssrf;
+
+pub use runtime_guard::{
+    GuardResult, RuntimeSecurityGuard, SecurityContext, SecurityGuardConfig, SecurityGuardError,
+};
+
+#[cfg(test)]
+mod export_tests {
+    #[test]
+    fn test_runtime_guard_exports_compile() {
+        let _ = crate::security::SecurityGuardConfig::default();
+        let _ = crate::security::RuntimeSecurityGuard::default_guard();
+    }
+}
