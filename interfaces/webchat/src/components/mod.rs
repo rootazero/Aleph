587764448b721aefa3696b10e7f1ba6b35ad8dd0
rootazero <zouguojunx@gmail.com>
2026-06-08 15:02:32 +0800

    panel: add unified ProviderBadges (default+verified coexist)

--- interfaces/webchat/src/components/mod.rs
+++ interfaces/webchat/src/components/mod.rs
@@ -17,6 +17,7 @@ pub mod mode_sidebar;
 pub mod model_picker;
 pub mod nav_menu;
 pub mod notification_center;
+pub mod provider_badge;
 pub mod provider_row_card;
 pub mod service_blocking_gate;
 pub mod session_tabs;
