3eabe2e650a3c9dca4f080e9b7b71db5d5cb0c25
rootazero <zouguojunx@gmail.com>
2026-08-21 16:39:49 +0800

    config: drop the unearned SpendPeriod re-export

--- src/config/types/policies/mod.rs
+++ src/config/types/policies/mod.rs
@@ -37,7 +37,7 @@ pub use exec_tier::{
 pub use memory::{CompressionPolicy, MemoryPolicies};
 pub use metrics::MetricsPolicy;
 pub use session_mode::{builtin_modes, SessionMode, MODE_SESSION_KEY};
-pub use spend::{SpendPeriod, SpendPolicy};
+pub use spend::SpendPolicy;
 pub use tool_permissions::{PermissionMatch, ToolPermissionsConfig};
 pub use web_fetch::{Crawl4aiConfig, WebFetchPolicy};
 
